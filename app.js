6a7f39a617b9c2454a327cf6d407444cb40a91d8
avalanche557 <abhinavkarna@Abhinavs-MacBook-Pro.local>
2020-05-14 01:56:11 +0530

    day 2 of 100

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ const bodyParser = require('body-parser');
 
 
 app.use(morgan('dev'))
+
 app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(bodyParser.json());
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', "*")
